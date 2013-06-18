67e841228e1c0db406a0c537af8d5382f51b5351
wrobin <wrobin@gmx.net>
2013-06-18 21:04:34 +0200

    new postgre version

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,8 @@ object ApplicationBuild extends Build {
     javaJdbc,
     "net.sf.flexjson" % "flexjson" % "2.1",
     javaEbean, // Use this for heroku deployment
-    "postgresql" % "postgresql" % "8.4-702.jdbc4" // Use this for heroku deployment
+    //"postgresql" % "postgresql" % "8.4-702.jdbc4" // Use this for heroku deployment
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
